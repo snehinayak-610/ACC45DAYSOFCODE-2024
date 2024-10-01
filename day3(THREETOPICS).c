ffbc7d2496e9e800bcee300757d9d8bb10137b7e
snehinayak-610 <snehilnayaksnu@gmail.com>
2024-10-01 18:18:56 +0100

    Add files via upload

---
+++ day3(THREETOPICS).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, C, X;
+    
+    // Reading the four integers A, B, C, and X
+    scanf("%d %d %d %d", &A, &B, &C, &X);
+    
+    // Check if X is equal to A, B, or C
+    if (X == A || X == B || X == C) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+
+    return 0;
+}
+
